180906d968d5bdd5f7834a490f26219384f21a48
Jaya9yellow <2010080026@klh.edu.in>
2022-11-10 19:40:00 +0530

    oops

--- Django3/settings.py
+++ Django3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0(-y7_4n6nd#!d-0^l(u-%7oqzdy9(d))1-k7%uxld93jcl#78
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jaya2010080026-ai.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
